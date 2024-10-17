c9d936c2f6fd4e747c1cc54e65c51f0edc0973bc
adri <aurquijom@uninorte.edu.co>
2024-10-17 15:38:06 -0500

    atributos Product

--- SweetDelicacies/src/core/sweetdelicacies/product/Product.java
+++ SweetDelicacies/src/core/sweetdelicacies/product/Product.java
@@ -4,18 +4,16 @@ package core.sweetdelicacies.product;
 import java.time.LocalDate;
 
 public abstract class Product {
-    String name;
-    float weight;
-    float unitPrice;
-    LocalDate fabricationDate;
+    
+    protected String name;
+    protected float weight;
+    protected float unitPrice;
+    protected LocalDate fabricationDate;
+    
     public Product(String name, float weight, float unitPrice, LocalDate fabricationDate) {
         this.name = name;
         this.weight = weight;
         this.unitPrice = unitPrice;
         this.fabricationDate = fabricationDate;
     }
-
-    
-
-    
 }
